342f87f5c588b7f47e07c07b862c52961844e6ab
nTony <jss9960@hotmail.com>
2016-04-13 02:42:12 -0700

    ignoring, idea folder

--- project/views.py
+++ project/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
-
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    template = loader.get_template('project/index.html')
+    return HttpResponse(template.render(request))
